2f00abdfcfa231518e4b33eacab608923d1158ae
langmf <atomix@mail.ru>
2024-05-09 21:33:56 +0400

    fix

--- files/example/positron.js
+++ files/example/positron.js
@@ -2,34 +2,26 @@
 
 const vscode = require("vscode");
 const path   = require("path");
-const prt    = path.dirname(module.parent.filename);
-const web    = require(`${prt}/webpanel`);
+const P      = path.dirname(module.parent.filename);
+
+const root   = require(`${P}\\root`);
+
+let context;
 
 
-let context, my1, my2;
+exports.activate    = (ctx)  => { context = ctx }
+exports.deactivate = async() => {}
 
 
-function newPage() {
-    const web2 = context.require('./webpanel')
-    my1 = new web2.WebPage({ file: __dirname + '/demo1.htm' });
-    my2 = new web2.WebPage({ file: __dirname + '/demo2.htm' });
-    my1.create();
-    my2.create();
-    //const timeout = setTimeout(() => exports.deactivate(), 3000);
-}
-
-
-exports.activate = (ctx) => {
-    context = ctx;
-}
-
-
-exports.deactivate = async() => {
-    await my1.web.panel.dispose();
-    await my2.web.panel.dispose();
-}
-
-
-exports.command = async (data, state) => {
-    if (data.action === 'program' && state) newPage();
+exports.command = async (data, state) =>
+{
+    if (data.action === 'compile' && state)
+    {
+        root.PS().beep(1000, 300);
+        await root.sleep(500);
+        await root.PS({ cwd: __dirname}).play('compile.wav');
+        
+        if (data.res.ok)  await root.PS().speak('Compilation completed successfully');
+        else              await root.PS().speak('Errors occurred during compilation');
+    }
 }
